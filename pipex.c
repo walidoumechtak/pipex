d6934443d0d3ff64c88d9d8aa65095950ab88992
Walid Oumechtak <woumecht@e1r5p10.1337.ma>
2023-01-18 16:22:11 +0100

    go ...

--- pipex.c
+++ pipex.c
@@ -6,33 +6,88 @@
 /*   By: woumecht <woumecht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 13:22:35 by woumecht          #+#    #+#             */
-/*   Updated: 2023/01/18 10:56:30 by woumecht         ###   ########.fr       */
+/*   Updated: 2023/01/18 14:38:45 by woumecht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-#include <string.h>
-#include <stdlib.h>
- #include <sys/wait.h>
- 
+
+void    ft_perror()
+{
+    perror("Failed to open file");
+    exit (0);
+}
+
+void    free_all(char **s)
+{
+    int i;
+    
+    i = 0;
+    while (s[i])
+    {
+        free(s[i]);
+        i++;
+    }
+    free(s);
+}
+
+char *path_cmd(char *str, char *cmd)
+{
+    int i;
+    char **p;
+    char *r;
+
+    p = ft_split(str, ':');
+    i = 0;
+    while (p[i])
+    {
+        if (access(p[i], F_OK) == 0)
+        {
+            r = p[i];
+            free_all(p);
+            return (r);
+        }
+        i++;
+    }
+    if (access(p[0], F_OK) == 0)
+    {
+        r =   p[i];
+        free_all(p);
+        return (r);  
+    }
+    free_all(p);
+    return (NULL);
+}
 
 int main(int ac, char **av, char **env)
 {
     int fd[2];
+    int fd_outfile;
+    int fd_infile;
     int pid;
     int stdout_copy;
-    char    
+    char **cmd1;
+
+    cmd1 = cmd1(av[2], env);
+    if (!path)
+        ft_perror();
+    if (pipe(fd) < 0)
+        return (1);
     stdout_copy = dup(1);
     if (ac == 5)
     {
         pid = fork();
         if (pid == 0)
         {
+            fd_infile = open(av[1], O_RDONLY);
+            if (fd_infile == -1)
+                ft_perror();
+            
             
         }
         else
         {
-            
+            fd_outfile = open(av[4], O_WRONLY | O_CREAT | O_TRUNC);
         }
     }
     else
--- pipex_utils.c
+++ pipex_utils.c
@@ -1,31 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   pipex_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: woumecht <woumecht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/15 13:23:03 by woumecht          #+#    #+#             */
-/*   Updated: 2023/01/18 11:19:11 by woumecht         ###   ########.fr       */
+/*   Created: 2023/01/18 10:58:07 by woumecht          #+#    #+#             */
+/*   Updated: 2023/01/18 14:38:11 by woumecht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-#define PIPEX_H
+#include "pipex.h"
 
-#include <stdio.h>
-#include <unistd.h>
-#include <time.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-
-
-char	**ft_split(char const *s, char c);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlen(const	char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-
-#endif
+char    **cmd1(char *cd1, char **env)
+{
+    char **s;
+    
+    s = ft_split(cd1, ' ');
+       
+}
